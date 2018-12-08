3cea1911cd21c964ef44825b1349a85a3443a551
Kevin Green <kevin.e.green@hotmail.com>
2018-12-08 14:57:27 -0600

    Portfolio Site

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,30 @@
      $('#status').fadeOut();
      $('#preloader').delay(350).fadeOut('slow');
  });
+
+ /* ================================
+            Preloader
+================================*/
+ $(function () { // Short form on ready method
+     $("#team-members").owlCarousel({
+         items: 2,
+         autoplay: true,
+         smartSpeed: 1000,
+         loop: true,
+         autoplayHoverPause: true,
+         nav: true,
+         dots: false,
+         navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+     });
+ });
+
+ /* ================================
+            Progress Bars
+================================*/
+$(function(){
+    $(".progress-bar").each(function(){
+        $(this).animate({
+            width: $(this).attr("aria-valuenow") + "%"
+        }, 1000);
+    });
+});
